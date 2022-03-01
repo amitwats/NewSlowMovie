19e77feadaa0ef639a108071ea98cdcc85b730e9
amitwats <amitwats@gmail.com>
2022-03-02 00:23:54 +1100

    Added hello click

--- testing/hello_click.py
+++ testing/hello_click.py
@@ -22,7 +22,7 @@ display = MyDisplay(vcom=DEFAULT_VCOM, rotate="CCW", spi_hz=24000000, flip=False
 
 # Setting GPIO Pins
 button_no = 40
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 GPIO.setup(button_no, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
