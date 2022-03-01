5ac7621115a58f4a9c017c526766718d99bae088
amitwats <amitwats@gmail.com>
2022-03-02 00:11:49 +1100

    Added hello click

--- testing/test_input.py
+++ testing/test_input.py
@@ -26,7 +26,7 @@ button_np_40 = 40
 GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(button_no_21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(button_np_40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button_np_40, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_np_40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(button_np_40, GPIO.IN)
 for _ in range(9):
     # state_21 = GPIO.input(button_no_21)
