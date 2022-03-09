7e4f792c7e69cb6aed2880af9bdb3be6c333685a
amitwats <amitwats@gmail.com>
2022-03-09 19:23:29 +1100

    Created Menu Page Selector

--- reader/menu_page_selector.py
+++ reader/menu_page_selector.py
@@ -62,7 +62,9 @@ class MenuPageSelector:
         image_draw.rectangle([self.RECT_BOX_X_START, self.RECT_BOX_Y_START, self.RECT_BOX_X_END, self.RECT_BOX_Y_END],
                              outline='black', fill='white')
         self.image_obj = ImageOps.mirror(self.image_obj)
-
+        paste_coords = [0, 0]
+        self.display.frame_buf.paste(self.image_obj, paste_coords)
+        self.display.draw_partial(constants.DisplayModes.GC16)
 
     # def get_current_selection(self):
     #     return self.list_books[self.selection_index]
